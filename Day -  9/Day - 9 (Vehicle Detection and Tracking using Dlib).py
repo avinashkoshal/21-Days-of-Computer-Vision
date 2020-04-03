849291cc659ab2bca5c56294379952a3da8ee56c
Avinash Koshal <31934725+aaavinash85@users.noreply.github.com>
2020-04-03 13:55:03 +0530

    Add files via upload

---
+++ Day -  9/Day - 9 (Vehicle Detection and Tracking using Dlib).py
@@ -0,0 +1,91 @@
+import cv2
+import dlib
+import time
+import threading
+import math
+
+carCascade = cv2.CascadeClassifier('myhaar.xml')
+video = cv2.VideoCapture('cars.mp4')
+WIDTH = 1280
+HEIGHT = 720
+
+def trackMultipleObjects():
+	rectangleColor = (0, 255, 0)
+	frameCounter = 0
+	currentCarID = 0
+	fps = 0
+	
+	carTracker = {}
+
+	
+	while True:
+		start_time = time.time()
+		rc, image = video.read()
+		if type(image) == type(None):
+			break
+		
+		image = cv2.resize(image, (WIDTH, HEIGHT))
+		resultImage = image.copy()
+		
+		frameCounter = frameCounter + 1
+		for carID in carTracker.keys():
+			trackingQuality = carTracker[carID].update(image)
+		
+		if not (frameCounter % 10):
+			gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+			cars = carCascade.detectMultiScale(gray, 1.1, 13, 18, (24, 24))
+
+			
+			for (_x, _y, _w, _h) in cars:
+				x = int(_x)
+				y = int(_y)
+				w = int(_w)
+				h = int(_h)
+			
+				x_bar = x + 0.5 * w
+				y_bar = y + 0.5 * h
+				
+				matchCarID = None
+			
+				for carID in carTracker.keys():
+					trackedPosition = carTracker[carID].get_position()
+					
+					t_x = int(trackedPosition.left())
+					t_y = int(trackedPosition.top())
+					t_w = int(trackedPosition.width())
+					t_h = int(trackedPosition.height())
+					
+					t_x_bar = t_x + 0.5 * t_w
+					t_y_bar = t_y + 0.5 * t_h
+				
+					if ((t_x <= x_bar <= (t_x + t_w)) and (t_y <= y_bar <= (t_y + t_h)) and (x <= t_x_bar <= (x + w)) and (y <= t_y_bar <= (y + h))):
+						matchCarID = carID
+				
+				if matchCarID is None:
+					print ('Creating new tracker ' + str(currentCarID))
+					
+					tracker = dlib.correlation_tracker()
+					tracker.start_track(image, dlib.rectangle(x, y, x + w, y + h))
+					
+					carTracker[currentCarID] = tracker
+					currentCarID = currentCarID + 1
+
+		for carID in carTracker.keys():
+			trackedPosition = carTracker[carID].get_position()
+					
+			t_x = int(trackedPosition.left())
+			t_y = int(trackedPosition.top())
+			t_w = int(trackedPosition.width())
+			t_h = int(trackedPosition.height())
+			
+			cv2.rectangle(resultImage, (t_x, t_y), (t_x + t_w, t_y + t_h), rectangleColor, 4)
+			
+		cv2.imshow('result', resultImage)
+		
+		if cv2.waitKey(33) == 27:
+			break
+	
+	cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+	trackMultipleObjects()
